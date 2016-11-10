b961c6ccde96035f2a9f9f5a487d43ae3ac15923
Filip_Keanu <knapikfilip@gmail.com>
2016-11-10 21:38:30 +0100

    Delete facedetect.js

--- www/facedetect.js
+++
@@ -1,7 +0,0 @@
-/*global cordova, module*/
-
-module.exports = {
-    startfacedetect: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "facedetect", "startfacedetect", [name]);
-    }
-};
